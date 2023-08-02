5945ecf4ed9aa33e5fa624ee5295f0659bf1e7c9
Lucas Manzatto <lucas_peres21@hotmail.com>
2023-08-02 19:26:45 -0300

    remove mysql reference

--- app/models/database.py
+++ app/models/database.py
@@ -1,15 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.engine.base import Engine
 from app.config import (
-    MYSQL_USERNAME,
-    MYSQL_PASSWORD,
-    MYSQL_HOST,
-    MYSQL_PORT,
-    MYSQL_DATABASE,
     POSTGRES_DATABASE,
     POSTGRES_HOST,
     POSTGRES_PASSWORD,
-    POSTGRES_PORT,
     POSTGRES_USERNAME,
 )
 
@@ -27,24 +21,9 @@ def get_connection(engine_type: str) -> str:
     Raises:
         ValueError: If the specified engine_type is not "mysql" or "postgres".
     """
-    if engine_type == "mysql":
-        return __get_mysql_connection()
     if engine_type == "postgres":
         return __get_postgres_connection()
     raise f"Engine type {engine_type} not implemented."
-
-
-def __get_mysql_connection() -> Engine:
-    """
-    Get a MySQL database engine connection.
-
-    Returns:
-        SQLAlchemy Engine: An SQLAlchemy Engine object representing the connection to the MySQL database.
-    """
-    connection_string = f"mysql+pymysql://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DATABASE}"
-
-    engine = create_engine(connection_string)
-    return engine
 
 
 def __get_postgres_connection() -> Engine:
